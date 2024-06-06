e377b3c077139e276eb7cc54bc560b3432d2c4a4
Daniel N <2color@users.noreply.github.com>
2024-06-06 17:59:20 +0200

    fix: circuit relay server in node.js

--- node-relay/index.js
+++ node-relay/index.js
@@ -9,11 +9,11 @@ import { gossipsub } from '@chainsafe/libp2p-gossipsub'
 import { webSockets } from '@libp2p/websockets'
 import { webRTC, webRTCDirect } from '@libp2p/webrtc'
 import { tcp } from '@libp2p/tcp'
-import { circuitRelayTransport } from '@libp2p/circuit-relay-v2'
 import { enable, disable } from '@libp2p/logger'
 import { pubsubPeerDiscovery } from '@libp2p/pubsub-peer-discovery'
 // import { update, getPeerTypes, getAddresses, getPeerDetails } from './utils'
 import { bootstrap } from '@libp2p/bootstrap'
+import { circuitRelayServer } from '@libp2p/circuit-relay-v2'
 
 async function main() {
   // enable('*')
@@ -21,26 +21,12 @@ async function main() {
     addresses: {
       listen: [
         '/ip4/0.0.0.0/tcp/9001/ws',
-        '/ip4/0.0.0.0/tcp/9002/',
-        '/webrtc'
+        '/ip4/0.0.0.0/tcp/9002',
       ],
     },
     transports: [
       webSockets(),
       tcp(),
-      circuitRelayTransport({
-        discoverRelays: 2
-      }),
-      webRTC({
-        rtcConfiguration: {
-          iceServers: [
-            {
-              // STUN servers help the browser discover its own public IPs
-              urls: ['stun:stun.l.google.com:19302', 'stun:global.stun.twilio.com:3478'],
-            },
-          ],
-        },
-      }),
     ],
     connectionEncryption: [noise()],
     streamMuxers: [yamux()],
@@ -51,10 +37,11 @@ async function main() {
     services: {
       identify: identify(),
       autoNat: autoNAT(),
+      relay: circuitRelayServer({})
     },
   })
 
-  libp2p.addEventListener('self:peer:update', (event) => console.log(event.detail.peer.addresses))
+  // libp2p.addEventListener('self:peer:update', (event) => console.log(event.detail.peer.addresses))
   console.log(libp2p.peerId.toString())
   console.log(libp2p.getMultiaddrs())
 }
